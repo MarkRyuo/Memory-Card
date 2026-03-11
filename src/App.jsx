75002ffb9228aa1e865954f7fcabc1dfb8f18f23
MarkRyuo <malupajhonmark3035@gmail.com>
2026-03-11 21:14:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
 import GameHeader from './components/GameHeader'
 
+const fruits = [
+  "🍇", "🍈", "🍉", "🍊", "🍋", "🍋‍🟩",
+  "🍇", "🍈", "🍉", "🍊", "🍋", "🍋‍🟩"
+]
+
+
+
 function App() {
 
   return (
     <>
       <main>
         <GameHeader score={1} moves={10}/>
+
+        <div>
+            {fruits.map((fruit) => {
+              return (
+                <div>{fruit}</div>
+              )
+            })}
+        </div>
       </main>
     </>
   )
