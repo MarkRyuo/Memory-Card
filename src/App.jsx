972298fca5755d8d6b8f60181f05b1e031f5a463
MarkRyuo <malupajhonmark3035@gmail.com>
2026-03-11 10:53:11 +0800

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import GameHeader from './components/GameHeader'
 
 function App() {
 
   return (
     <>
       <main>
-
+        <GameHeader score={1} moves={10}/>
       </main>
     </>
   )
