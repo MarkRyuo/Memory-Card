d7e38aba46cdb5bc0b71cc1229161c58a09bbdd8
MarkRyuo <malupajhonmark3035@gmail.com>
2026-03-11 22:07:43 +0800

    Subject: Update

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,17 @@ function App() {
 
   return (
     <>
-      <main className='flex justify-center items-center flex-col'>
-        <GameHeader score={1} moves={10}/>
+      <main className='flex justify-center items-center flex-col w-full h-screen' style={{ background: "radial-gradient(125% 125% at 50% 10%, #fff 40%, #6366f1 100%)", }}>
+        <GameHeader score={1} moves={10} />
 
-        <div className='w-[500px] h-auto grid grid-cols-3 grid-rows-3 gap-0.5'>
-            {fruits.map((fruit) => {
-              return (
-                <div className=''>
-                  <div className=' h-20 flex justify-center items-center border-[1px] text-4xl'>{fruit}</div>
-                </div>
-              )
-            })}
+        <div className='w-125 h-auto grid grid-cols-3 grid-rows-3 gap-0.5'>
+          {fruits.map((fruit) => {
+            return (
+              <div className=''>
+                <div className=' h-20 flex justify-center items-center border text-4xl'>{fruit}</div>
+              </div>
+            )
+          })}
         </div>
       </main>
     </>
--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -5,9 +5,9 @@ import React from 'react'
 function GameHeader({score, moves}) {
   return (
     <div>
-      <h1 className='text-2xl p-[5px]'>🎮 Memory Card Game</h1>
+      <h1 className='text-2xl p-1.25'>🎮 Memory Card Game</h1>
 
-      <div className='flex justify-between p-[10px]'>
+      <div className='flex justify-between p-2.5'>
         <div>
         <span>Score:</span>
         <span>{score}</span>
